868d8814ce68986b178053c606cb742b3f9e6d81
AmrutB <amrutb@winjit.com>
2015-06-24 20:33:39 +0530

    updated commit

--- app/src/main/java/com/winjit/myapp/MainActivity.java
+++ app/src/main/java/com/winjit/myapp/MainActivity.java
@@ -1,17 +1,81 @@
 package com.winjit.myapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rclvwList = (RecyclerView)findViewById(R.id.xrclvwList);
+    LinearLayoutManager layoutManager;
+    private int previousTotal = 0;
+    private boolean loading = true;
+    private int visibleThreshold = 5;
+    RVListAdapter rvListAdapter;
+    ArrayList<String> items=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layoutManager=new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        rclvwList.setLayoutManager(layoutManager);
+
+        addItemsToList();
+
+        rvListAdapter=new RVListAdapter(items);
+        rclvwList.setAdapter(rvListAdapter);
+
+        rclvwList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                int visibleItemCount = rclvwList.getChildCount();
+                int totalItemCount = layoutManager.getItemCount();
+                int firstVisibleItem = layoutManager.findFirstVisibleItemPosition();
+
+                if (loading) {
+                    if (totalItemCount > previousTotal) {
+                        loading = false;
+                        previousTotal = totalItemCount;
+                    }
+                }
+                if (!loading && (totalItemCount - visibleItemCount)
+                        <= (firstVisibleItem + visibleThreshold)) {
+                    // End has been reached
+
+                    Log.i("...", "end called");
+
+                    // Do something
+                    Toast.makeText(MainActivity.this,"end called",Toast.LENGTH_SHORT).show();
+                    loading = true;
+                }
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
+
+    }
+
+    private void addItemsToList()
+    {
+        for (int i=0;i<100;i++)
+        {
+            items.add("item "+i);
+        }
     }
 
 
